ea5c8560efce19ceaa5930b0bc95c91af6142861
Prdsilva80 <prdsilva717@gmail.com>
2023-04-14 19:42:31 -0300

    + info e ajustes no layout

--- src/components/UserList.tsx
+++ src/components/UserList.tsx
@@ -5,12 +5,36 @@ type User = {
   name: {
     first: string;
     last: string;
+    title: string;
   };
   gender: string;
   location: {
     country: string;
+    street: {
+      name: string;
+      number: number;
+    };
+    city: string;
+    state: string;
+    postcode: string;
+    coordinates: {
+      latitude: string;
+      longitude: string;
+    };
+    timezone: {
+      offset: string;
+      description: string;
+    };
   };
+  email: string;
+  login: {
+    username: string;
+    password: string;
+  };
+  phone: string;
   picture: {
+    large: string;
+    medium: string;
     thumbnail: string;
   };
 };
@@ -20,27 +44,34 @@ const UserList: React.FC = () => {
   const [users, setUsers] = useState<User[]>([]);
 
   useEffect(() => {
-    axios.get<{ results: User[] }>("https://randomuser.me/api/?results=10").then((response) => {
-      setUsers(response.data.results);
-    });
+    axios
+      .get<{ results: User[] }>("https://randomuser.me/api/?results=10")
+      .then((response) => {
+        setUsers(response.data.results);
+      });
   }, []);
 
   return (
-    <div>
+    <div className="user-list">
       <h1>Lista de Usuários</h1>
-      <ul>
-      {users.map((user, index) => (
-  <li key={index}>
-    <img src={user.picture.thumbnail} alt={user.name.first} />
-    <div>
-      <p>{user.name.first} {user.name.last},</p>
-      <p>{user.gender},</p>
-      <p>{user.location.country}</p>
-    </div>
-  </li>
-))}
-
-      </ul>
+      <div className="user-card-list">
+        {users.map((user, index) => (
+          <div key={index} className="user-card">
+            <div className="user-avatar">
+              <img src={user.picture.medium} alt={user.name.first} />
+            </div>
+            <div className="user-info">
+              <h2>{`${user.name.title} ${user.name.first} ${user.name.last}`}</h2>
+              <p>{user.email}</p>
+              <p>{user.phone}</p>
+              <p>
+                {`${user.location.street.number} ${user.location.street.name},`}
+              </p>
+              <p>{`${user.location.city}, ${user.location.state}, ${user.location.country}, ${user.location.postcode}`}</p>
+            </div>
+          </div>
+        ))}
+      </div>
     </div>
   );
 };
